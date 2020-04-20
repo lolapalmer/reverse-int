0b1d952d4afe28c61e8cef0891eb9f058fa4aa02
lolapalmer <64013528+lolapalmer@users.noreply.github.com>
2020-04-20 15:03:12 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+  if (b < 0){
+      return -reverse(-b)
+  };
+  let str = n.toString()
+  let strArray = str.split("");
+  let revArray = strArray.reverse();
+  let rev = revArray.join("");
+  return Number(rev);
 }
